771ee07bb9e82b07fba89cb04b5a71ffcdccde24
maderisza <55948902+maderisza@users.noreply.github.com>
2019-09-29 22:38:16 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("hello nama saya<Made Risza Kartika> dari mata kuliah komputasi awan 2019 ITTELKOM PURWOKERTO
-      
-SyntaxError: EOL while scanning string literal
->>> 
->>> 
->>> print("hello nama saya<Made Risza Kartika> dari mata kuliah komputasi awan 2019 ITTELKOM PURWOKERTO")
-hello nama saya<Made Risza Kartika> dari mata kuliah komputasi awan 2019 ITTELKOM PURWOKERTO
->>> 
+print('made')
